a0c00be3e6a66073616411de483a9d552cde697c
Dennis Lang <>
2019-01-08 09:09:35 -0500

    Fix shortcut making/removing on newer OS

--- app/src/main/java/com/landenlabs/all_devtool/shortcuts/ShortcutUtil.java
+++ app/src/main/java/com/landenlabs/all_devtool/shortcuts/ShortcutUtil.java
@@ -3,7 +3,12 @@ package com.landenlabs.all_devtool.shortcuts;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
+import android.os.Build;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.landenlabs.all_devtool.DevToolActivity;
 import com.landenlabs.all_devtool.FileBrowserFragment;
@@ -13,34 +18,46 @@ import com.landenlabs.all_devtool.PackageFragment;
 import com.landenlabs.all_devtool.R;
 import com.landenlabs.all_devtool.ScreenFragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Dennis Lang on 6/13/2015.
  */
 public class ShortcutUtil {
 
     public static void updateShortcutFileBrowser(boolean makeIt) {
-        updateShortcut(makeIt, FileBrowserFragment.s_name, "DS FileBrowser", R.drawable.shortcut_fb);
+        updateShortcut(makeIt, false, FileBrowserFragment.s_name, "DS FileBrowser", R.drawable.shortcut_fb);
     }
 
     public static void updateShortcutGPS(boolean makeIt) {
-        updateShortcut(makeIt, GpsFragment.s_name, "DS GPS", R.drawable.shortcut_gps);
+        updateShortcut(makeIt, false, GpsFragment.s_name, "DS GPS", R.drawable.shortcut_gps);
     }
 
     public static void updateShortcutPackage(boolean makeIt) {
-        updateShortcut(makeIt, PackageFragment.s_name, "DS Package", R.drawable.shortcut_pkg);
+        updateShortcut(makeIt, true, PackageFragment.s_name, "DS Package", R.drawable.shortcut_pkg);
     }
 
     public static void updateShortcutScreen(boolean makeIt) {
-        updateShortcut(makeIt, ScreenFragment.s_name, "DS Screen", R.drawable.shortcut_scn);
+        updateShortcut(makeIt, false, ScreenFragment.s_name, "DS Screen", R.drawable.shortcut_scn);
     }
 
     @SuppressWarnings("ConstantConditions")
     public static void makeShortcuts() {
         boolean makeIt = true;
+    //     activeShortcutInfos = new ArrayList<>(4);
         ShortcutUtil.updateShortcutFileBrowser(makeIt);
         ShortcutUtil.updateShortcutGPS(makeIt);
         ShortcutUtil.updateShortcutPackage(makeIt);
         ShortcutUtil.updateShortcutScreen(makeIt);
+
+        /*
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ShortcutManager shortcutManager = GlobalInfo.s_globalInfo.mainFragActivity
+                    .getSystemService(ShortcutManager.class);
+            shortcutManager.addDynamicShortcuts(activeShortcutInfos);
+        }
+        */
     }
 
     @SuppressWarnings("ConstantConditions")
@@ -52,8 +69,10 @@ public class ShortcutUtil {
         ShortcutUtil.updateShortcutScreen(makeIt);
     }
 
+    // static List<ShortcutInfo> activeShortcutInfos;
+
     // http://stackoverflow.com/questions/6424246/creating-shortcuts-in-android-via-intent
-    public static void updateShortcut(boolean makeIt, String fragName, String shortcutName, int iconResID) {
+    public static void updateShortcut(boolean makeIt, boolean pinShortcut, String fragName, String shortcutName, int iconResID) {
 
         FragmentActivity fragActivity = GlobalInfo.s_globalInfo.mainFragActivity;
 
@@ -67,26 +86,58 @@ public class ShortcutUtil {
         shortcutIntent = new Intent(fragActivity, DevToolActivity.class);
         shortcutIntent.putExtra(GlobalInfo.STARTUP_FRAG, fragName);
         shortcutIntent.setAction(Intent.ACTION_MAIN);
-        // shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        // shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        // shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
 
-        final Intent putShortCutIntent = new Intent();
-        putShortCutIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-        putShortCutIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, shortcutName);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            final Intent putShortCutIntent = new Intent();
+            putShortCutIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+            putShortCutIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, shortcutName);
 
-        // Requires permission
-        //  <uses-permission android:name= "com.android.launcher.permission.UNINSTALL_SHORTCUT"/>
-        putShortCutIntent.setAction("com.android.launcher.action.UNINSTALL_SHORTCUT");
-        fragActivity.sendBroadcast(putShortCutIntent);
-
-        if (makeIt) {
             // Requires permission
-            //  <uses-permission android:name="com.android.launcher.permission.INSTALL_SHORTCUT" />
-            putShortCutIntent.putExtra("duplicate", false);
-            putShortCutIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, Intent.ShortcutIconResource.fromContext(fragActivity, iconResID));
-            putShortCutIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+            //  <uses-permission android:name= "com.android.launcher.permission.UNINSTALL_SHORTCUT"/>
+            putShortCutIntent.setAction("com.android.launcher.action.UNINSTALL_SHORTCUT");
             fragActivity.sendBroadcast(putShortCutIntent);
+
+            if (makeIt) {
+                // Requires permission
+                //  <uses-permission android:name="com.android.launcher.permission.INSTALL_SHORTCUT" />
+                putShortCutIntent.putExtra("duplicate", false);
+                putShortCutIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
+                        Intent.ShortcutIconResource.fromContext(fragActivity, iconResID));
+                putShortCutIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+                fragActivity.sendBroadcast(putShortCutIntent);
+            }
+
+        } else {
+            ShortcutManager shortcutManager = fragActivity.getSystemService(ShortcutManager.class);
+            if (makeIt) {
+                // https://stackoverflow.com/questions/46796674/create-shortcut-on-home-screen-in-android-o
+                ShortcutInfo.Builder mShortcutInfoBuilder = new ShortcutInfo.Builder(fragActivity, fragName);
+                mShortcutInfoBuilder.setShortLabel(fragName);
+                mShortcutInfoBuilder.setLongLabel(fragName);
+                mShortcutInfoBuilder.setIcon(Icon.createWithResource(fragActivity, iconResID));
+                mShortcutInfoBuilder.setIntent(shortcutIntent);
+
+                ShortcutInfo mShortcutInfo = mShortcutInfoBuilder.build();
+                if (pinShortcut) {
+                    shortcutManager.requestPinShortcut(mShortcutInfo, null);
+                } else {
+                    List<ShortcutInfo> activeShortcutInfos = new ArrayList<>(1);
+                    activeShortcutInfos.add(mShortcutInfo);
+                    if (!shortcutManager.addDynamicShortcuts(activeShortcutInfos)) {
+                        Log.w("Shortcut", "Failed to add shortcut for " + fragName);
+                    }
+                }
+            } else {
+                List<ShortcutInfo> activeShortcutInfos = shortcutManager.getDynamicShortcuts();
+                List<String> removeIds = new ArrayList<>();
+                for (ShortcutInfo shortcutInfo : activeShortcutInfos) {
+                    if (shortcutInfo.getShortLabel().equals(fragName)) {
+                        removeIds.add(shortcutInfo.getId());
+                        break;
+                    }
+                }
+                shortcutManager.removeDynamicShortcuts(removeIds);
+            }
         }
 
     //    sharedPreferences.edit().putBoolean(shortcutName, true);
