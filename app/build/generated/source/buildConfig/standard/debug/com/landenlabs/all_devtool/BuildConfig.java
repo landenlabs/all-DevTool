88a94bf417f8679feb735c7763b849df71f28a4b
landenlabs2 <landenlabs2@gmail.com>
2020-07-18 13:55:13 -0400

    update to latest build

---
+++ app/build/generated/source/buildConfig/standard/debug/com/landenlabs/all_devtool/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.landenlabs.all_devtool;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.landenlabs.all_devtool";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "standard";
+  public static final int VERSION_CODE = 30714;
+  public static final String VERSION_NAME = "3.07.14";
+}
